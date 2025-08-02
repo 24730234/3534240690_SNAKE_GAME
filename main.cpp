1a1234fd35ce52b5bf93c1f7358f355f17ce4cce
24730190 <24730190@ms.uit.edu.vn>
2025-08-02 16:14:44 +0700

    update 4h14pm ngay 2 thang 8

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@ const int Moi = 10; //so luong moi
 
 // Khai bao bien toan cuc
 int x, y; // toa do cua ran
-int Score; // diem so
+int DiemSo; // diem so
 int nDot; // so luong dot ran
 int DotX[100], DotY[100]; // Mang luu toa do cua dot ran
 int MoiX, MoiY; // toa do moi
 int MoiDaAn; // kiem tra so moi da an
 bool GameOver = false ; // kiem tra ket thuc tro choi
+bool MoiHienTai = false; // kiem tra moi hien tai
 
 // Ham sinh moi
 void SinhMoi() {
@@ -30,6 +31,47 @@ void KhoiTao() {
     MoiDaAn = 0;
     SinhMoi();
     GameOver = false;
+}
+// Ham ve giao dien
+void VeGiaoDien() {
+    system("cls"); // xoa man hinh console
+    for (int i = 0; i < width + 2; i++) cout << "#"; // ve duong vien
+    cout << endl;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+        if (j == 0) cout <<"#";
+        bool print = flase;
+        // In dau ran
+        if (i == y && j == x) {
+            cout << char(248); // hinh tron
+            print = true;
+        }
+        // In than ran
+        else {
+            for (int k = 0; k < nDot; k++) {
+                if (DotX[k] == j && DotY[k] == i) {
+                    cout << char(248); // hinh tron
+                    print = true;
+                    break;
+                }
+            }
+        }               
+        // In moi
+        if (!print && MoiHienTai && MoiX == j && MoiY == i) {
+            cout << char(248); // hinh vuong
+            print = true;
+        }
+        // In khoang trang
+        if (!print) cout << " ";
+        if (j == width - 1) cout << "#"; // ve duong vien
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < width + 2; i++) cout << "#"; // ve duong vien
+    cout << endl;
+    cout << "Diem so: " << DiemSo << endl; // hien thi diem so  
+    cout << "So moi da an: " << MoiDaAn << "/" << Moi << endl; // hien thi so moi da an
+    cout << "Chieu dai con ran: " << nDot + 1   << endl; // hien thi chieu dai con ran
 }
 
 
