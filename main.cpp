06b4f40a10feae8106fd17565af3be2a732c53eb
24730190 <24730190@ms.uit.edu.vn>
2025-08-02 11:06:29 +0700

    Hoang Hai

--- main.cpp
+++ main.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <ctime> // dung cho srand
+#include <window.h> // dung cho Sleep
+using namespace std;
+const int width = 20; //chieu rong
+const int height = 20; //chieu cao
+const int Moi = 10; //so luong moi
+
+// Khai bao bien toan cuc
+int x, y; // toa do cua ran
+int Score; // diem so
+int nDot; // so luong dot ran
+int DotX[100], DotY[100]; // Mang luu toa do cua dot ran
+int MoiX, MoiY; // toa do moi
+int MoiDaAn; // kiem tra so moi da an
+bool GameOver = false ; // kiem tra ket thuc tro choi
+
+// Ham sinh moi
+void SinhMoi() {
+    MoiX = rand() % width;
+    MoiY = rand() % height;
+    MoiDaAn = 0; // reset moi da an
+}
+// Ham khoi tao tro choi
+void KhoiTao() {
+    x = width / 2;
+    y = height / 2;
+    Score = 0;
+    nDot = 0;
+    SinhMoi();
+    GameOver = false;
+}
 int main() {
     return 0;
 }
