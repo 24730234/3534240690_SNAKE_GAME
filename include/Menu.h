e7ee36a82bac88cbeb9702d37e3339c9f367e958
24730206.TranTrungKien <24730206@ms.uit.edu.vn>
2025-08-08 18:47:39 +0700

    Add Menu.h and Menu.cpp function showStartMenu() stay in Menu.h

---
+++ include/Menu.h
@@ -0,0 +1,22 @@
+#ifndef MENU_H
+#define MENU_H
+#include <vector>
+#include <conio.h>
+#include "Control.h"
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+extern vector<string> menu;
+
+/// @brief Hàm in menu
+/// @param menu các lựa chọn trong menu
+/// @param selectedIndex vị trí đang được lựa chọn
+void printMenu(vector<string>& menu, int& selectedIndex);
+
+int returnSelectedIndex(Control control, char key, int& selectedIndex);
+
+void showStartMenu();
+
+#endif // MENU_H
---
+++ src/Menu.cpp
@@ -0,0 +1,77 @@
+#include "Menu.h"
+
+vector<string> menu = {"Play", "Settings", "Exit"};
+
+void printMenu(vector<string>& menu, int& selectedIndex)
+{   
+    system("cls");
+    cout << "------------Menu------------" << endl;
+    for(int i = 0; i < menu.size(); i++)
+    {   
+        if (selectedIndex == i)
+        {
+            cout << "\033[41m\033[37m" << menu[i] << "\033[0m" << endl;
+        }
+        else 
+        {
+            cout << menu[i] << endl;
+        }
+        
+    }
+}
+
+int returnSelectedIndex(Control control, char key, int& selectedIndex)
+{   
+    key = _getch(); // Lấy phím bấm không cần Enter
+        control.updateDirectionMenu(key);
+        if (control.getDirection() != NONINPUT) // Kiểm tra có ấn phím khác ngoài các phím chức năng
+        {
+            if (control.getDirection() == ENTER ) // Enter
+            { 
+                return selectedIndex; 
+            }
+            else if (control.getDirection() == ESC) // ESC
+            {   
+                return 2;
+            }
+            // Điều khiển lên/xuống dựa vào enum Direction của bạn
+            if (control.getDirection() == UP || control.getDirection() == LEFT) 
+            {
+                selectedIndex--;
+                if (selectedIndex < 0) selectedIndex = menu.size() - 1;
+            } 
+            else if 
+            (control.getDirection() == DOWN || control.getDirection() == RIGHT)
+            {
+                selectedIndex++;
+                if (selectedIndex >= menu.size()) selectedIndex = 0;
+            }
+            
+        }
+    return -1;
+}
+
+void showStartMenu()
+{
+    Control control;
+    int selectedIndex = 0, choose = -1;
+
+    char key;
+    while (true) {
+        printMenu(menu, selectedIndex);
+        choose = returnSelectedIndex(control, key, selectedIndex); 
+        if (choose == 0) {
+            cout << "Game Start" << endl;
+            // gameStart() se o day
+            system("pause");
+        }
+        else if (choose == 2)
+        {
+            cout << "Goodbye" << endl;
+            // thoat game se o day
+            system("pause");
+            break;
+        }
+         
+    }
+}
