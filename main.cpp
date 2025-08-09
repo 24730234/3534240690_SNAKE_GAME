2ee62c0fe03740ab6b9ae9a371135800753f656d
24730190 <24730190@ms.uit.edu.vn>
2025-08-09 22:43:43 +0700

    giao dien

--- main.cpp
+++ main.cpp
@@ -52,6 +52,161 @@ void resetSnake();// Hàm khôi phục vị trí của rắn về ban đầu.
 #pragma region Định nghĩa hàm
 // Mọi người viết hàm ở đây nhé.
 
+
+// Vẽ khung trò chơi
+void drawBox()
+{
+    for (size_t i = 0; i < WIDTH; i++)
+        cout << '=';
+    gotoxy(0, HEIGHT);
+    for (size_t i = 0; i < WIDTH; i++)
+        cout << '=';
+    for (size_t i = 1; i < HEIGHT; i++)
+    {
+        gotoxy(0, i);
+        cout << '|';
+    }
+    for (size_t i = 1; i < HEIGHT; i++)
+    {
+        gotoxy(WIDTH, i);
+        cout << '|';
+    }
+}
+// Hiển thị điểm số
+void displayScore()
+{
+    gotoxy(WIDTH + 5, 2);
+    cout << "Diem so cua ban " << score;
+}
+
+// Hiển thị menu kết thúc trò chơi
+void showEndMenu()
+{
+    system("cls");
+    gotoxy(0, 0);
+    cout << "Ket thuc tro choi!" << endl;
+    cout << "Diem cua ban: " << score << endl;
+    cout << "Ban co muon choi nua khong ([y]/[n]): ";
+    char option;
+    cin >> option;
+    option = tolower(option);
+    if (option == 'y')
+    {
+        resetSnake();
+        startGame();
+    }
+    else if (option == 'n')
+        exit(1);
+}
+
+// Bắt đầu trò chơi
+void startGame()
+{
+    system("cls");
+    ShowConsoleCursor(false);
+    drawBox();
+    drawSnake();
+    genApple();
+    displayScore();
+    while (true)
+    {
+        if (_kbhit())
+        {
+            char ch = _getch();
+            if (ch == -32 || ch == 0xE0) // Arrow key prefix
+            {
+                ch = _getch(); // Get the second byte
+                switch (ch)
+                {
+                case 72: // Up arrow
+                    if (direction != Direction::down)
+                        direction = Direction::up;
+                    break;
+                case 77: // Right arrow
+                    if (direction != Direction::left)
+                        direction = Direction::right;
+                    break;
+                case 80: // Down arrow
+                    if (direction != Direction::up)
+                        direction = Direction::down;
+                    break;
+                case 75: // Left arrow
+                    if (direction != Direction::right)
+                        direction = Direction::left;
+                    break;
+                }
+            }
+            else if (ch == 'q') // Thoát trò chơi
+            {
+                showEndMenu();
+                break;
+            }
+        }
+        move();
+        drawHeadnTail();
+        if (isAteApple())
+        {
+            score++;
+            displayScore();
+            growing();
+            genApple();
+        }
+        if (isBiteItself())
+        {
+            ShowConsoleCursor(true);
+            showEndMenu();
+            break;
+        }
+        if (isHitWall())
+        {
+            ShowConsoleCursor(true);
+            showEndMenu();
+            break;
+        }
+        Sleep(speed);
+    }
+}
+// Hiển thị menu bắt đầu trò chơi
+void showStartMenu()
+{
+    system("cls");
+    cout << "\t===== Ran San Moi====!" << endl;
+    cout << "\tTuy chinh:" << endl;
+    cout << "\t1. Bat Dau" << endl;
+    cout << "\t2. Ket thuc" << endl;
+    cout << "\tLua chon cua ban: ";
+    int option;
+    cin >> option;
+    if (option == 1)
+    {
+        system("cls");
+        cout << "Chon cap do choi (1 - 5): ";
+        int t;
+        cin >> t;
+        speed = 600 - t * 100; // Tính tốc độ dựa trên cấp độ
+        system("cls");
+        cout << "Meo: khi choi game, ban co the nhan 'q' de thoat" << endl;
+        gotoxy(0, 2);
+        cout << "San sang!";
+        Sleep(1000);
+        for (size_t i = 3; i > 0; i--)
+        {
+            system("cls");
+            cout << "Meo: khi choi game, ban co the nhan 'q' de thoat" << endl;
+            gotoxy(0, 2);
+            cout << "Dem nguoc: " << i;
+            Sleep(1000);
+        }
+        system("cls");
+        cout << "Meo: khi choi game, ban co the nhan 'q' de thoat" << endl;
+        gotoxy(0, 2);
+        cout << "Choi !";
+        Sleep(1000);
+        startGame();
+    }
+    else if (option == 2)
+        exit(1);
+}
 #pragma endregion
 
 int main() {
